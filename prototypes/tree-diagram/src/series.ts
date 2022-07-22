4a688c017c07601ce3bfb2e492df626cbaba7578
vsvensso <vsvensso@tibco.com>
2022-07-22 15:02:41 +0200

    font styling added and node border bug solved

--- prototypes/tree-diagram/src/series.ts
+++ prototypes/tree-diagram/src/series.ts
@@ -1,9 +1,14 @@
-import {Column, DataViewHierarchyNode, MarkingOperation } from "spotfire-api";
+import {Column, MarkingOperation } from "spotfire-api";
 
+export enum NodeType {
+    Internal = "node-internal",
+    Leaf = "node-leaf"
+}
 export interface Node {
     // id: string;
     value?: string;
     width?: number;
+    type?: NodeType,
     // mark(mode?: MarkingOperation): void;
     children?: Node[];
 }
@@ -18,12 +23,13 @@ export function buildNodeSeries(
     let childNodes : Node[] = nodes.map((node) => ({
         // id: node.,
         value: node.name,
-        width: node.name.length*fontSize*0.7
+        width: node.name.length*fontSize*0.7,
+        type: NodeType.Leaf
         //mark: (m) => node.mark(m)
     }));
 
     //Fake build
-    let nodeSeries : Node[] = [{ value: "SuperStore", width: "SuperStore".length*fontSize, children: childNodes}]
+    let nodeSeries : Node[] = [{ value: "SuperStore", width: "SuperStore".length*fontSize, type: NodeType.Internal, children: childNodes}]
 
     return nodeSeries;
 }
