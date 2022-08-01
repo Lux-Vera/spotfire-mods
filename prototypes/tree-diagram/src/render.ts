73e5e1daeb3c7f0c88a71d8129c02f59ba48b3fc
tobiashjalmarsson <tobhja@student.chalmers.se>
2022-08-01 15:37:19 +0200

    Removing zoom on dubbelclick to not interfere with removing branches

--- prototypes/tree-diagram/src/render.ts
+++ prototypes/tree-diagram/src/render.ts
@@ -493,7 +493,7 @@ function handleZoom() {
  * Calls the zoom on the svg
  */
 function initZoom(zoom: any) {
-    d3.select("svg").call(zoom);
+    d3.select("svg").call(zoom).on("dblclick.zoom", null);
 }
 
 export function singleClick(d: any, update: any, tooltip: Tooltip, f: FontInfo) {
